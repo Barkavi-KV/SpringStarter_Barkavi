1c98f1544a2f18dc7320efd3ab9c0bf71ca723a7
Barkavi Asokan <abarkavi@kognivera.com>
2024-02-21 18:31:41 +0530

    Created Spring Test Project

---
+++ src/test/java/com/test/SpringTest_Barkavi/SpringTestBarkaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.SpringTest_Barkavi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestBarkaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
